6602992b4244dd386c42d2efe0a1eb89ed6d6036
odinsubject09 <your-email@example.com>
2025-05-27 16:56:55 +0530

    add getWatchHistory and getUserChannelProfile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,10 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/apiError.js'
 import { User } from '../models/user.model.js'
-import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import { uploadOnCloudinary,deleteFromCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import jwt from "jsonwebtoken"
+import mongoose from 'mongoose'
 
 const generateAccessTokenAndRefreshToken=async(userId)=>{
     try{
@@ -227,9 +228,253 @@ const refreshAccessToken=asyncHandler(async(req,res)=>
         throw new ApiError(401,error?.message)
     }
 })
+
+const changeCurrentPassword=asyncHandler(async(req,res)=>
+    {
+    const{oldPassword,newPassword,}=req.body
+    const user=await User.findById(req.user?._id)
+    
+    const isPasswordCorrect=user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect)
+    {
+        throw new ApiError(400,"Invalid Password")
+    }
+
+    user.password=newRefreshToken
+    await user.save({
+        validateBeforeSave:false
+    })
+
+    return res.
+    status(200).
+    json(new ApiResponse(200,{},"Password is changed"))
+})
+
+const getCurrentUser=asyncHandler(async(req,res)=>{
+    return res.
+           status(200).
+           json(new ApiResponse(200,req.user,"current user fetched successfully"))
+
+})
+
+const updateAccountDetails=asyncHandler(async(req,res)=>{
+    const{fullName,email}=req.body
+    if(!fullName||!email)
+    {
+        throw new ApiError(400,"both fields required")
+    }
+    const user=await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullName,
+                email:email
+            }
+        },
+        {
+            new:true
+        }
+    ).select("-password")
+
+    return res.status(200).
+    json(new ApiResponse(200,user,"user details updated"))
+})
+
+const updateUserAvatar=asyncHandler(async(req,res)=>{
+    const avatarLocalPath=req.file?.path
+
+    if(!avatarLocalPath)
+    {
+        throw new ApiError(400,"Avatar is missing")
+    }
+
+    const avatar=await uploadOnCloudinary(avatarLocalPath)
+    if(!avatar.url)
+    {
+        throw new ApiError(400,"cannot create avatar url")
+    }
+    const oldAvatarPath=req.user.avatar
+    const user=await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },
+        {new:true}
+    ).select("-password")
+
+    const response=await deleteFromCloudinary(oldAvatarPath)
+    return res.status(200)
+            .json(new ApiResponse(200,user,"avatar updated successfully")) 
+})
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Cover image updated successfully")
+    )
+})
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    const {username}=req.params
+
+    if(!username?.trim())
+    {
+        throw new ApiError(400,"username is missing")
+    }
+    const channel=await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscriberCount:
+                {
+                    $size:"$subscribers"
+                },
+                subscribedToCount:
+                {
+                    $size:"$subscribedTo"
+                },
+                isSubscribedTo:
+                {
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                coverImage:1,
+                avatar:1,
+                email:1,
+                subscribedToCount:1,
+                subscriberCount:1,
+                isSubscribedTo:1
+            }
+        }
+    ])
+    if(!channel?.length)
+    {
+        throw new ApiError(404,"Channel does not exist")
+    }
+    return res.status(200).
+            json(new ApiResponse(200,channel[0],"User channel found successfully"))
+})
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+})
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    updateAccountDetails,
+    getCurrentUser,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,45 @@
 import {v2 as cloudinary} from 'cloudinary'
 import fs from 'fs'
+import { ApiError } from './apiError.js';
 
 cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
+
+function getPublicIdFromUrl(url) {
+  const parts = url.split('/');
+  const uploadIndex = parts.indexOf('upload');
+  if (uploadIndex === -1 || uploadIndex + 1 >= parts.length) return null;
+
+  const relevantParts = parts.slice(uploadIndex + 1);
+
+  // Remove version like v123456789
+  if (/^v\d+$/.test(relevantParts[0])) {
+    relevantParts.shift();
+  }
+
+  const filePath = relevantParts.join('/');
+  return filePath.replace(/\.[^/.]+$/, ''); // remove file extension
+}
+
+const deleteFromCloudinary = async (url) => {
+  try {
+    const public_id = getPublicIdFromUrl(url);
+    if (!public_id) {
+      throw new ApiError(400,'Invalid Cloudinary URL or public_id could not be extracted');
+    }
+
+    const result = await cloudinary.uploader.destroy(public_id);
+    console.log('Deletion result:', result);
+    return result;
+  } 
+  catch (error) {
+    console.error('Error deleting from Cloudinary:', error);
+    throw new ApiError(500, "Could not delete URL");
+  }
+}
 
 const uploadOnCloudinary=async(filePath)=>
 {
@@ -30,4 +64,6 @@ const uploadOnCloudinary=async(filePath)=>
     }
 }
 
-export {uploadOnCloudinary}
+
+export {uploadOnCloudinary,
+        deleteFromCloudinary}
